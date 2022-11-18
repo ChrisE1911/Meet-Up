6e997ebfed3c1bc5f52c2e87ed792dbf474743a4
Christopher Eatmon <eatmonchristopher1@gmail.com>
2022-11-18 12:44:59 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,6 +31,22 @@ router.post(
         user
       });
     }
+);
+
+// backend/routes/api/session.js
+// ...
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
   );
+
+  // ...
+
+
 
 module.exports = router;
