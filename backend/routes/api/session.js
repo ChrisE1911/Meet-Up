d3d240cd63051a768d7bcbf087e4d616607b2fea
Christopher Eatmon <eatmonchristopher1@gmail.com>
2022-11-26 23:48:28 -0600

    Changed functionality in Log in and sign up user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -21,7 +21,7 @@ router.post(
     async (req, res, next) => {
       const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+      let user = await User.login({ credential, password });
 
       if (!user) {
         const err = new Error('Login failed');
@@ -33,8 +33,10 @@ router.post(
 
       await setTokenCookie(res, user);
 
+      user = user.toJSON();
+
       return res.json({
-        user
+        ...user
       });
     }
 );
