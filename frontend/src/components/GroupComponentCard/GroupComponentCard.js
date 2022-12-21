6ba71a724f798ede6620ae007ce16ed2f7cf6b0d
Christopher Eatmon <eatmonchristopher1@gmail.com>
2022-12-20 18:57:03 -0600

    Changed Css for group cards

--- frontend/src/components/GroupComponentCard/GroupComponentCard.js
+++ frontend/src/components/GroupComponentCard/GroupComponentCard.js
@@ -7,11 +7,14 @@ const GroupComponentCard = ({ group }) => {
     // console.log('single group object', group)
     return (
         <Link to={`/groups/${group.id}`} className='group-component-card'>
+            <div className='group-component-card-image'>
             <img src={group.previewImage} alt='Group' style={{width: '100px'}} ></img>
+            </div>
+            <div className='group-component-card-details'>
             <h3>{group.name}</h3>
             <p>{group.about}</p>
-
             <p>{`${group.numMembers} members ${group.private ? "Private" : "Public"}`}</p>
+            </div>
         </Link>
     )
 }
